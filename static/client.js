d1e7cbe1bdd8272c467d60164bc2f667ed190ea9
Paul Gowder <paultopia@users.noreply.github.com>
2018-03-10 19:05:33 -0600

    fix miscommit

--- static/client.js
+++ static/client.js
@@ -1,3 +1,47 @@
+var root = document.body;
+
+
+var AddWorkoutPage = {view: function(){
+  return m("section", {class: "section"},
+           m("div", {class: "container"}, "Add workout form goes here")
+          )}};
+
+var AddAccountPage = {view: function(){
+  return m("section", {class: "section"},
+           m("div", {class: "container"}, "Add account form goes here")
+          )}};
+
+var ViewWorkoutsPage = {view: function(){
+  return m("section", {class: "section"},
+           m("div", {class: "container"}, "View workout page goes here")
+          )}};
+
+var HomePage = {view: homePageViewFunction};
+// this is a vicious hack to take advantage of function hoisting
+// the function that refers to this page is on bottom
+// otherwise I get terrible circular reference problems.
+
+var homeRoutes = {
+  "home": HomePage,
+  "add-account": AddAccountPage,
+  "add-workout": AddWorkoutPage,
+  "view-workouts": ViewWorkoutsPage
+}
+
+
+
+// home screen
+var AddLift = {view: function(){
+  return m("div", {class: "title is-child notification is-primary"}, m("a", {href: "#!add-workout"},"Add a Workout"))
+}};
+
+var AddAccount = {view: function(){
+  return m("div", {class: "title is-child notification is-info"}, "Add Account")
+}};
+
+var ViewWorkouts = {view: function(){
+  return m("div", {class: "title is-child notification is-link"}, "View Workouts")
+}};
 
 var HomeButton = {view: function(vnode){
   var route = "#!" + vnode.attrs.route;
@@ -30,8 +74,11 @@ var MenuBox = {view: function(){
 
 function homePageViewFunction(){
   return m("section", {class: "section"},
-           m("div", {class: "container"}, m(MenuBox))
+           m("div", {class: "container"}, m(NewMenuBox))
           )}
+//THIS HAS TO STAY AT BOTTOM TO HAVE ALL THE VARIABLES
+// NEEDED FOR MENUBOX.  AND GETS HOISTED BECAUSE 
+// ONE OF THOSE VARIABLES REFERS TO THIS FUNCTION.
 
 
 m.route(root, "home", homeRoutes)
